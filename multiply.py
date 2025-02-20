6faa670e2f28d33a9f8f1b31e3df11fce537fa7a
bfpead1 <blake.pead@gmail.com>
2025-02-20 10:18:03 -0700

    Create multiplyTwoNumbers Function
    Wrote a function that accepts two numbers and multiplies

---
+++ multiply.py
@@ -0,0 +1,7 @@
+# Blake Pead
+# Section 003
+# 2/20/25
+#Description: Accept two numbers and return the product
+def multiplyTwoNmbers(iNum1,iNum2):
+    #Multiply and return the result
+    return iNum1*iNum2
